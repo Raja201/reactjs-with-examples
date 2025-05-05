ca610d28f6b9ea3b72b20cef4a335e2112309867
Raja Sahu <rajasahu2246@gmail.com>
2025-05-05 10:59:37 +0530

    Basic1

---
+++ Basic1/Components/README.md
@@ -0,0 +1,15 @@
+# Profile Card - JSX & Components
+
+This is a simple profile card project demonstrating how JSX and React Components work.
+
+## Topics Covered
+- Functional Components
+- JSX syntax
+- Styling with JS objects
+
+## Preview
+
+## Run it Locally
+
+npm install
+npm run dev
---
+++ Basic1/Components/src/App.jsx
@@ -0,0 +1,12 @@
+import ProfileCard from "./components/ProfileCard";
+import "./App.css"
+function App() {
+  return (
+    <div className="container">
+      <h1 style={{ textAlign: "center" }}>JSX & Components: Profile Card</h1>
+      <ProfileCard />
+    </div>
+  );
+}
+
+export default App;
